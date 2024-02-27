ab1172e011853e8a3db002572dbb1fc9b7c40d6a
Andreas Walter <andreas.walter@rwth-aachen.de>
2024-02-27 08:25:25 +0100

    acados include dir added to cmakelists.txt

--- src/trajectory_optimization_node.cpp
+++ src/trajectory_optimization_node.cpp
@@ -7,6 +7,14 @@
 #include <trajectory_optimization/trajectory_optimization_node.hpp>
 
 #include <rclcpp_components/register_node_macro.hpp>
+
+// acados
+#include "acados/utils/print.h"
+#include "acados_c/ocp_nlp_interface.h"
+#include "acados_c/external_function_interface.h"
+#include "acados/ocp_nlp/ocp_nlp_constraints_bgh.h"
+#include "acados/ocp_nlp/ocp_nlp_cost_ls.h"
+
 RCLCPP_COMPONENTS_REGISTER_NODE(trajectory_optimization::TrajectoryOptimizationNode)
 
 
