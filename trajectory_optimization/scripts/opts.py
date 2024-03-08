5673fe9fd2f59343b7f06912bb7699662a2ba4a9
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2024-03-08 11:58:43 +0000

    remove scripts from trajectory_optimization package

--- trajectory_optimization/scripts/opts.py
+++
@@ -1,9 +0,0 @@
-from acados_template import AcadosOcpOptions
-
-def export_opts() -> AcadosOcpOptions:
-    
-    opts = AcadosOcpOptions()
-
-    opts.tf = 5.0 # prediction horizon s
-
-    return opts
