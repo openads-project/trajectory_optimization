8bd06c8a93156962ed2a2f73a53d2190be7ba717
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-03-15 14:45:11 +0100

    move python files back to trajectory_optimization package

--- trajectory_optimization/acados_ocp/generate_ocp.py
+++ trajectory_optimization/acados_ocp/generate_ocp.py
@@ -1,4 +1,4 @@
-from acados_template import AcadosOcp, AcadosOcpSolver
+from acados_template import AcadosOcp, AcadosOcpSolver, builders
 import model, dims, constraints, costs, opts
 
 def main():
@@ -9,8 +9,9 @@ def main():
     ocp.constraints = constraints.export_constraints()
     ocp.cost = costs.export_costs()  
     ocp.solver_options = opts.export_opts()
+    ocp.code_export_directory = 'c_generated_code'
 
-    acados_tp_ocp = AcadosOcpSolver(ocp, json_file = 'acados_tp_ocp.json', simulink_opts=None, build=False, generate=True)
+    acados_tp_ocp = AcadosOcpSolver(ocp, json_file = 'acados_tp_ocp.json', simulink_opts=None, build=True, generate=True, cmake_builder=builders.CMakeBuilder())
 
 if __name__ == '__main__':
     main()
