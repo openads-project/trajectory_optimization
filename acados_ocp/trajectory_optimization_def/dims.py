cd6a0f599fbdd266df553fe4262cd68a58584711
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2024-03-20 15:54:41 +0100

    improve dims

--- acados_ocp/trajectory_optimization_def/dims.py
+++ acados_ocp/trajectory_optimization_def/dims.py
@@ -5,7 +5,7 @@ def export_dims(model, parameters) -> AcadosOcpDims:
     dims = AcadosOcpDims()
 
     dims.N = parameters['nsteps'] # number of shooting intervals
-    dims.nx = model.x.size()[0] # number of states
-    dims.nu = model.u.size()[0] # number of inputs/controls
+    dims.nx = model.x.rows() # number of states
+    dims.nu = model.u.rows() # number of inputs/controls
 
     return dims
