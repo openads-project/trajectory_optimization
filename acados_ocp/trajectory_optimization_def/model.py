244071d8769e26cf24f6d2f1e9ef7e8b6183f518
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2024-03-20 14:48:06 +0000

    fix model

--- acados_ocp/trajectory_optimization_def/model.py
+++ acados_ocp/trajectory_optimization_def/model.py
@@ -1,5 +1,5 @@
 from acados_template import AcadosModel
-from casadi import SX, vertcat, sin, cos
+from casadi import SX, vertcat, sin, cos, tan
 
 def export_model(parameters) -> AcadosModel:
     
@@ -16,10 +16,10 @@ def export_model(parameters) -> AcadosModel:
     y      = SX.sym('y')
     s      = SX.sym('s')
     v      = SX.sym('v')
-    a      = SX.sym('a')
+    a_lon  = SX.sym('a_lon')
     psi    = SX.sym('psi')
     delta  = SX.sym('delta')
-    state = vertcat(x, y, s, v, a, psi, delta)
+    state = vertcat(x, y, s, v, a_lon, psi, delta)
 
     # set up controls
     j_lon = SX.sym('j_lon')
@@ -31,20 +31,20 @@ def export_model(parameters) -> AcadosModel:
     y_dot = SX.sym('y_dot')
     s_dot = SX.sym('s_dot')
     v_dot = SX.sym('v_dot')
-    a_dot = SX.sym('a_dot')
+    a_lon_dot = SX.sym('a_lon_dot')
     psi_dot = SX.sym('psi_dot')
     delta_dot = SX.sym('delta_dot')
-    state_dot = vertcat(x_dot, y_dot, s_dot, v_dot, a_dot, psi_dot, delta_dot)
+    state_dot = vertcat(x_dot, y_dot, s_dot, v_dot, a_lon_dot, psi_dot, delta_dot)
 
     # dynamics
     f_x_dot = v*cos(psi)
     f_y_dot = v*sin(psi)
     f_s_dot = v
-    f_v_dot = a
-    f_a_dot = j_lon
-    f_psi_dot = v/l*sin(delta)
+    f_v_dot = a_lon
+    f_a_lon_dot = j_lon
+    f_psi_dot = v/l*tan(delta)
     f_delta_dot = alpha
-    f_expl = vertcat(f_x_dot, f_y_dot, f_s_dot, f_v_dot, f_a_dot, f_psi_dot, f_delta_dot)
+    f_expl = vertcat(f_x_dot, f_y_dot, f_s_dot, f_v_dot, f_a_lon_dot, f_psi_dot, f_delta_dot)
 
     # dynamics: implicit DAE
     model.f_impl_expr = state_dot - f_expl
