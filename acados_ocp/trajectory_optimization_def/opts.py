892f9935dd6b3c04b8dfdb8d0681d386694cb81a
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2024-03-20 15:15:52 +0000

    adapt opts

--- acados_ocp/trajectory_optimization_def/opts.py
+++ acados_ocp/trajectory_optimization_def/opts.py
@@ -5,11 +5,10 @@ def export_opts(parameters) -> AcadosOcpOptions:
     opts = AcadosOcpOptions()
 
     # set options
-    opts.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
+    opts.qp_solver = 'FULL_CONDENSING_QPOASES' # FULL_CONDENSING_QPOASES
     # PARTIAL_CONDENSING_HPIPM, FULL_CONDENSING_QPOASES, FULL_CONDENSING_HPIPM,
     # PARTIAL_CONDENSING_QPDUNES, PARTIAL_CONDENSING_OSQP, FULL_CONDENSING_DAQP
     opts.hessian_approx = 'GAUSS_NEWTON' # 'GAUSS_NEWTON', 'EXACT'
-    opts.integrator_type = 'IRK'
     # opts.print_level = 1
     opts.nlp_solver_type = 'SQP' # SQP_RTI, SQP
 
