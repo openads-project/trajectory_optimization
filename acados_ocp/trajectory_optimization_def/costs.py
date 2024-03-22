42e92fa1b1dcabcf04c2bf01c466a4379414d4f3
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2024-03-22 15:01:01 +0000

    rewrite dvel term according to nietzschmann

--- acados_ocp/trajectory_optimization_def/costs.py
+++ acados_ocp/trajectory_optimization_def/costs.py
@@ -1,5 +1,5 @@
 from acados_template import AcadosOcpCost
-from casadi import vertcat
+from casadi import vertcat, fmax
 import numpy as np
 
 def set_costs(ocp, parameter):    
@@ -9,9 +9,13 @@ def set_costs(ocp, parameter):
     cost.cost_type = 'NONLINEAR_LS'
     cost.cost_type_e = 'NONLINEAR_LS'
 
-    ocp.model.cost_y_expr = vertcat(ocp.model.x[0], ocp.model.x[1], ocp.model.x[3])
-    ocp.model.cost_y_expr_0 = vertcat(ocp.model.x[0], ocp.model.x[1], ocp.model.x[3])
-    ocp.model.cost_y_expr_e = vertcat(ocp.model.x[0], ocp.model.x[1], ocp.model.x[3])
+    v_term = (ocp.model.p[0] - ocp.model.x[3]) / fmax(ocp.model.p[0], 2.78) # (v_ref - v)/max(v_ref, 2.78)
+
+    ocp.parameter_values = np.array([0])
+
+    ocp.model.cost_y_expr = vertcat(ocp.model.x[0], ocp.model.x[1], v_term)
+    ocp.model.cost_y_expr_0 = vertcat(ocp.model.x[0], ocp.model.x[1], v_term)
+    ocp.model.cost_y_expr_e = vertcat(ocp.model.x[0], ocp.model.x[1], v_term)
 
     cost.yref = np.array([0, 0, 0])
     cost.yref_0 = np.array([0, 0, 0])
