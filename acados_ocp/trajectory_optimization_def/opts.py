34498cab0fa7264af8a839d29f25e838261c56e7
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-04-30 12:15:49 +0000

    use ERK integrator type and increase default frequency

--- acados_ocp/trajectory_optimization_def/opts.py
+++ acados_ocp/trajectory_optimization_def/opts.py
@@ -7,7 +7,7 @@ def set_opts(ocp, config):
     # set options
     opts.qp_solver = 'PARTIAL_CONDENSING_HPIPM' #'FULL_CONDENSING_QPOASES'
     opts.hessian_approx = "GAUSS_NEWTON"
-    opts.integrator_type = "IRK"
+    opts.integrator_type = "ERK"
     opts.nlp_solver_type = "SQP"
 
     # set prediction horizon in s
