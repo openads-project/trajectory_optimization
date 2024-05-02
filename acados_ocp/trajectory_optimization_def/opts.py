8213c462015b1debd500876b9687ec10485fc997
Fabian Thomsen <fabian.thomsen@ika.rwth-aachen.de>
2024-05-02 15:53:26 +0200

    Add def. opts, build acados in Release

--- acados_ocp/trajectory_optimization_def/opts.py
+++ acados_ocp/trajectory_optimization_def/opts.py
@@ -9,6 +9,13 @@ def set_opts(ocp, config):
     opts.hessian_approx = "GAUSS_NEWTON"
     opts.integrator_type = "ERK"
     opts.nlp_solver_type = "SQP"
+    opts.nlp_solver_max_iter = 100  # default
+    opts.nlp_solver_tol_stat = 1e-6 # default
+    opts.qp_solver_iter_max = 50    # default
+    opts.qp_solver_warm_start = 0   # default
+    opts.sim_method_num_stages = 4  # default
+    opts.sim_method_num_steps = 1   # default
+
 
     # set prediction horizon in s
     opts.tf = config['optimization_horizon']
