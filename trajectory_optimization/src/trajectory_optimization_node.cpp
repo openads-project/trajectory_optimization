12da7939c2d065760a9a4a5e12fbbee588c38228
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-05-16 14:50:32 +0000

    better handling of failed interpolation

--- trajectory_optimization/src/trajectory_optimization_node.cpp
+++ trajectory_optimization/src/trajectory_optimization_node.cpp
@@ -755,8 +755,8 @@ void TrajectoryOptimizationNode::setOcpParameters(std::vector<double>& cost_weig
           Y.push_back(perception_msgs::object_access::getY(predicted_state));
         }
         double des_time = rclcpp::Time(ego_data.header.stamp).nanoseconds() / 1e9 + dt * i;
-        if (!linearInterpolation(TIME, X, des_time, x_tgt)) return;
-        if (!linearInterpolation(TIME, Y, des_time, y_tgt)) return;
+        if (!linearInterpolation(TIME, X, des_time, x_tgt)) break;
+        if (!linearInterpolation(TIME, Y, des_time, y_tgt)) break;
       }
 
       obstacles[p_obstacles_shape_[1] * i + 0] = x_tgt;
