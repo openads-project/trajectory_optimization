c168f8208bf7c8216562a7ee283f4d60ca83e1b4
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2024-05-17 08:19:54 +0200

    update default of cost shape

--- trajectory_optimization/include/trajectory_optimization/trajectory_optimization_node.hpp
+++ trajectory_optimization/include/trajectory_optimization/trajectory_optimization_node.hpp
@@ -153,11 +153,11 @@ class TrajectoryOptimizationNode : public rclcpp::Node {
   trajectory_planning_msgs::msg::Trajectory latest_valid_trajectory_;
 
   // cost weights
-  std::vector<double> cost_weights_ = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+  std::vector<double> cost_weights_ = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
   double dynamic_weight_ = 1.1;
 
   // ocp parameter vector structure
-  std::vector<long int> p_cost_weights_shape_ = {10, 1};
+  std::vector<long int> p_cost_weights_shape_ = {13, 1};
   std::vector<long int> p_ref_path_shape_ = {100, 4};
   std::vector<long int> p_obstacles_shape_ = {10, 3};
 
