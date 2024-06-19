165004a6e411a2218d3d7bd973d461229fb8fabc
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-06-19 10:54:22 +0000

    build with qpoases

--- docker/custom.sh
+++ docker/custom.sh
@@ -2,7 +2,7 @@
 git clone --recurse-submodules https://github.com/acados/acados.git /opt/acados
 mkdir -p /opt/acados/build
 cd /opt/acados/build
-cmake -DCMAKE_BUILD_TYPE=Release .. # -DACADOS_WITH_QPOASES=ON -DACADOS_WITH_QORE=ON -DACADOS_WITH_OOQP=ON -DACADOS_WITH_QPDUNES=ON -DACADOS_WITH_OSQP=ON -DACADOS_WITH_OPENMP=ON
+cmake -DCMAKE_BUILD_TYPE=Release -DACADOS_WITH_QPOASES=ON .. # -DACADOS_WITH_QORE=ON -DACADOS_WITH_OOQP=ON -DACADOS_WITH_QPDUNES=ON -DACADOS_WITH_OSQP=ON -DACADOS_WITH_OPENMP=ON
 make install -j8
 
 # install acados python interface
