e8a602716e25c8f8edc40084cda492de5d0cd05a
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-12-20 12:25:20 +0100

    set acados version to latest commit

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,5 +1,5 @@
 # clone acados repo and build it
-git clone --branch v0.4.0 --recurse-submodules https://github.com/acados/acados.git /opt/acados
+git clone --recurse-submodules https://github.com/acados/acados.git /opt/acados
 mkdir -p /opt/acados/build
 cd /opt/acados/build
 cmake -DCMAKE_BUILD_TYPE=Release .. # -DACADOS_WITH_QPOASES=ON -DACADOS_WITH_QORE=ON -DACADOS_WITH_OOQP=ON -DACADOS_WITH_QPDUNES=ON -DACADOS_WITH_OSQP=ON -DACADOS_WITH_OPENMP=ON
