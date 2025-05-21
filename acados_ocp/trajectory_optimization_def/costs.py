aec96fdc651ed4a6784a8444082ee6b7af65b83e
Jan Bergmann <jan.bergmann@ika.rwth-aachen.de>
2025-05-21 12:05:57 +0000

    interpolated v cost restored

--- acados_ocp/trajectory_optimization_def/costs.py
+++ acados_ocp/trajectory_optimization_def/costs.py
@@ -38,16 +38,17 @@ def set_costs(ocp: AcadosOcp, config):
     w_psi = p_cost_weights[1]
     w_x = p_cost_weights[2]
     w_y = p_cost_weights[3]
-    w_v_t = p_cost_weights[4]
-    w_obstacles = p_cost_weights[5]
-    w_a_n = p_cost_weights[6]
-    w_a_t_pos = p_cost_weights[7]
-    w_a_t_neg = p_cost_weights[8]
-    w_j_n = p_cost_weights[9]
-    w_j_t_pos = p_cost_weights[10]
-    w_j_t_neg = p_cost_weights[11]
-    w_alpha = p_cost_weights[12]
-    w_end_yaw = p_cost_weights[13]
+    w_v_t_interpolated = p_cost_weights[4]
+    w_v_t = p_cost_weights[5]
+    w_obstacles = p_cost_weights[6]
+    w_a_n = p_cost_weights[7]
+    w_a_t_pos = p_cost_weights[8]
+    w_a_t_neg = p_cost_weights[9]
+    w_j_n = p_cost_weights[10]
+    w_j_t_pos = p_cost_weights[11]
+    w_j_t_neg = p_cost_weights[12]
+    w_alpha = p_cost_weights[13]
+    w_end_yaw = p_cost_weights[14]
 
     # other cost params
     p_thw = p_cost_params[0]
@@ -66,6 +67,8 @@ def set_costs(ocp: AcadosOcp, config):
     #### define costs at intermediate nodes
     # reference path costs
     ocp.model.cost_expr_ext_cost = p_dynamic_weight * w_lat * ref_path_costs["dlat"]
+    ocp.model.cost_expr_ext_cost += p_dynamic_weight * w_v_t_interpolated * ref_path_costs["v_t_interpolated"]
+
 
     # reference point costs
     ocp.model.cost_expr_ext_cost += p_dynamic_weight * w_x * ref_point_costs["x"]
@@ -154,10 +157,6 @@ def calc_interpolated_ref_path_cost(ocp: AcadosOcp, config: dict, p_ref_path: ca
 
     # lateral deviation term
     dlat_term = ca.power(dlat, 2) / ca.power(config["c_lat"], 2)
-    # x deviation term
-    x_term = ca.power((ocp.model.x[STATE_INDEX_X] - x_ref), 2) / ca.power(config["c_x"], 2)
-    # y deviation term
-    y_term = ca.power((ocp.model.x[STATE_INDEX_Y] - y_ref), 2) / ca.power(config["c_y"], 2)
     # v deviation term
     # first ensure that the reference velocity is > 0
     v_ref = ca.fmax(v_ref_inter, 0.0)
@@ -168,7 +167,7 @@ def calc_interpolated_ref_path_cost(ocp: AcadosOcp, config: dict, p_ref_path: ca
     # psi deviation term
     psi_term = ca.power(wrap_angle(ocp.model.x[STATE_INDEX_PSI] - psi_ref_inter), 2) / ca.power(config["c_psi"], 2)
 
-    cost_terms = {"dlat": dlat_term, "psi": psi_term}
+    cost_terms = {"dlat": dlat_term, "psi": psi_term, "v_t_interpolated": v_term}
     return cost_terms
 
 def calc_abs_ref_point_cost(ocp: AcadosOcp, config: dict, p_ref_point: ca.MX) -> dict:
@@ -181,7 +180,9 @@ def calc_abs_ref_point_cost(ocp: AcadosOcp, config: dict, p_ref_point: ca.MX) ->
     # y deviation term
     y_term = ca.power((ocp.model.x[STATE_INDEX_Y] - y_ref), 2) / ca.power(config["c_y"], 2)
     # v deviation term
-    v_term = ca.power((ocp.model.x[STATE_INDEX_V_T] - v_ref), 2) / ca.power(config["c_v"], 2)
+    v_ref = ca.fmax(v_ref, 0.0)
+    v_scale = ca.fmax(v_ref, V_SCALE_MIN)
+    v_term = ca.power((ocp.model.x[STATE_INDEX_V_T] - v_ref), 2) / ca.power(v_scale, 2)
 
     cost_terms = {"x": x_term, "y": y_term, "v_t": v_term}
     return cost_terms
