5f61b9b04d0e069078bcc781d795b7cacb8d606b
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2025-05-30 09:36:34 +0000

    improve launchfile

--- trajectory_optimization/launch/trajectory_optimization.launch.py
+++ trajectory_optimization/launch/trajectory_optimization.launch.py
@@ -20,54 +20,38 @@ def generate_launch_description_with_resolved_launch_args(launch_context):
         raise ValueError(f"Invalid driving mode: {driving_mode}")
 
     # define other launch arguments / remappings / nodes
-    params_arg = DeclareLaunchArgument('params', default_value=PathJoinSubstitution([
-        get_package_share_directory("trajectory_optimization"), "config", "params.yml"])
-    )
-
-    node_name_arg = DeclareLaunchArgument('node_name', default_value=executable_name)
-    namespace_arg = DeclareLaunchArgument('namespace', default_value='')
-
-    drivable_space_topic_arg = DeclareLaunchArgument('drivable_space_topic', default_value='~/drivable_space')
-    ego_data_topic_arg = DeclareLaunchArgument('ego_data_topic', default_value='~/ego_data')
-    object_list_topic_arg = DeclareLaunchArgument('object_list_topic', default_value='~/object_list')
-    reference_trajectory_topic_arg = DeclareLaunchArgument('reference_trajectory_topic', default_value='~/reference_trajectory')
-    route_topic_arg = DeclareLaunchArgument('route_topic', default_value='~/route')
-
-    trajectory_topic_arg = DeclareLaunchArgument('trajectory_topic', default_value='~/trajectory')
-    object_circles_topic_arg = DeclareLaunchArgument('object_circles_topic', default_value='~/visualization/object_circles')
-
-    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='False')
+    
+    remappable_topics = [
+        DeclareLaunchArgument("ego_data_topic", default_value="~/ego_data"),
+        DeclareLaunchArgument("object_list_topic", default_value="~/object_list"),
+        DeclareLaunchArgument("reference_trajectory_topic", default_value="~/reference_trajectory"),
+        DeclareLaunchArgument("route_topic", default_value="~/route"),
+        DeclareLaunchArgument("trajectory_topic", default_value="~/trajectory"),
+        DeclareLaunchArgument("object_circles_topic", default_value="~/visualization/object_circles"),
+    ]
+    
+    args = [
+        DeclareLaunchArgument("name", default_value=executable_name, description="node name"),
+        DeclareLaunchArgument("namespace", default_value="", description="node namespace"),
+        DeclareLaunchArgument("params", default_value=os.path.join(get_package_share_directory("trajectory_optimization"), "config", "params.yml"), description="path to parameter file"),
+        DeclareLaunchArgument("log_level", default_value="info", description="ROS logging level (debug, info, warn, error, fatal)"),
+        DeclareLaunchArgument("use_sim_time", default_value="false", description="use simulation clock"),
+        *remappable_topics
+    ]
 
     return [
-        params_arg,
-        node_name_arg,
-        namespace_arg,
-        drivable_space_topic_arg,
-        ego_data_topic_arg,
-        object_list_topic_arg,
-        reference_trajectory_topic_arg,
-        route_topic_arg,
-        trajectory_topic_arg,
-        object_circles_topic_arg,
-        use_sim_time_arg,
-        SetParameter(name='use_sim_time', value=LaunchConfiguration('use_sim_time')),
+        *args,
+        SetParameter(name="use_sim_time", value=LaunchConfiguration("use_sim_time")),
         Node(
             package="trajectory_optimization",
             executable=executable_name,
-            name=LaunchConfiguration('node_name'),
-            namespace=LaunchConfiguration('namespace'),
+            name=LaunchConfiguration("name"),
+            namespace=LaunchConfiguration("namespace"),
+            parameters=[LaunchConfiguration("params")],
+            arguments=["--ros-args", "--log-level", LaunchConfiguration("log_level")],
+            remappings=[(la.default_value[0].text, LaunchConfiguration(la.name)) for la in remappable_topics],
             output="screen",
             emulate_tty=True,
-            parameters=[LaunchConfiguration('params')],
-            remappings=[
-                ("~/drivable_space", LaunchConfiguration('drivable_space_topic')),
-                ("~/ego_data", LaunchConfiguration('ego_data_topic')),
-                ("~/object_list", LaunchConfiguration('object_list_topic')),
-                ("~/reference_trajectory", LaunchConfiguration('reference_trajectory_topic')),
-                ("~/route", LaunchConfiguration('route_topic')),
-                ("~/trajectory", LaunchConfiguration('trajectory_topic')),
-                ("~/visualization/object_circles", LaunchConfiguration('object_circles_topic'))
-            ]
         )
     ]
 
