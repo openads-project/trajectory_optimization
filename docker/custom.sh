d952c6ff8f2776b6871579e9e31bbdc5e6e431f5
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2026-07-20 12:14:54 +0200

    tmp switch to acados fork for timeout

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,7 +1,7 @@
 # clone acados repo and build it
-git clone --recurse-submodules https://github.com/acados/acados.git /opt/acados
+git clone --recurse-submodules https://github.com/jpbusch/acados.git /opt/acados
 cd /opt/acados
-git checkout v0.5.5
+git checkout sqp-wfqp-timeout
 git submodule update --init --recursive
 mkdir -p /opt/acados/build
 cd /opt/acados/build
