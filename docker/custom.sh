5c0ca868f57d10a761758ccc6f4d381f9c82d365
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-09-04 12:21:49 +0200

    Merge pull request #23 from openads-project/tracing
    Fix installation of ros2_tracing

--- docker/custom.sh
+++ docker/custom.sh
@@ -28,7 +28,7 @@ ldconfig
 # 'message-link instrumentation' and 'dual-session mode' in jazzy
 cd /docker-ros/ws
 git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
-rosdep update && rosdep install -y -i --from-paths src/ros2_tracing/tracetools src/ros2_tracing/tracetools_launch
+rosdep update && rosdep install -y -i --from-paths src/ros2_tracing
 source /opt/ros/${ROS_DISTRO}/setup.bash
-colcon build --packages-up-to tracetools tracetools_launch --allow-overriding tracetools --allow-overriding tracetools_launch
+colcon build --cmake-args '-DCMAKE_BUILD_TYPE=Release' --allow-overriding tracetools --allow-overriding tracetools_launch
 rm -r src/ros2_tracing log build
